3fbb0f4c0470aebca31e82e9932cf4864fe09069
Coded-Adil <itsadil2022@gmail.com>
2024-11-03 21:05:12 +0600

    sorting functions done

--- src/pages/Coffees.jsx
+++ src/pages/Coffees.jsx
@@ -1,14 +1,37 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "../components/Card";
+import { useState } from "react";
 
 const Coffees = () => {
     const data = useLoaderData();
+    const [coffees, setCoffees] = useState(data);
+    const handleSort = (sortBy) => {
+        if (sortBy == 'popularity') {
+            const sorted = [...data].sort((a, b) => b.popularity - a.popularity);
+            setCoffees(sorted);
+        }
+        else if (sortBy == 'rating') {
+            const sorted = [...data].sort((a, b) => a.rating - b.rating);
+            setCoffees(sorted);
+        }
+    }
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
-            {
-                data.map(coffee => <Card key={coffee.id} coffee={coffee} />)
-            }
-        </div>
+        <>
+            <div className="flex justify-between items-center my-12">
+                <div>
+                    <h2 className="text-3xl font-thin">Sort Coffee&apos;s by Popularity & Rating-&gt;</h2>
+                </div>
+                <div className="space-x-4">
+                    <button onClick={() => handleSort('popularity')} className="btn btn-warning">Sort By Popularity (DSC)</button>
+                    <button onClick={() => handleSort('rating')} className="btn btn-warning">Sort By Rating (ASC)</button>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
+                {
+                    coffees.map(coffee => <Card key={coffee.id} coffee={coffee} />)
+                }
+            </div>
+        </>
     );
 };
 
