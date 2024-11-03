a0d2ef74f661d4ec42c2888c9db0178058f2644b
Coded-Adil <itsadil2022@gmail.com>
2024-11-03 18:35:02 +0600

    showing all coffee datas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,40 @@
+import { Link } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 const Card = ({ coffee }) => {
     const { name, image, category, origin, type, id, rating, popularity } = coffee || {}
     return (
-        <div>
-            {name}
+        <div className="flex relative">
+            <Link
+                to={`/coffee/${id}`}
+                className="transition hover:scale-105 shadow-xl rounded-xl overflow-hidden"
+            >
+                <figure className="w-full h-48 overflow-hidden">
+                    <img
+                        src={image}
+                        alt={name} />
+                </figure>
+                <div className="p-4 space-y-2">
+                    <h2 className="text-2xl font-thin">
+                        Name: {name}
+                    </h2>
+                    <p>
+                        Category: {category}
+                    </p>
+                    <p>
+                        Type: {type}
+                    </p>
+                    <p>
+                        Origin: {origin}
+                    </p>
+                    <p>
+                        Rating: {rating}
+                    </p>
+                    <p>
+                        Popularity: {popularity}
+                    </p>
+                </div>
+            </Link>
         </div>
     );
 };
