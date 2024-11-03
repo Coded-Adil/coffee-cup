0a052536c6b7f4a9ef9a74ddce834d81daabea77
Coded-Adil <itsadil2022@gmail.com>
2024-11-03 19:38:31 +0600

    Navbar done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
     return (
-        <div className="hero bg-banner bg-base-200 min-h-[600px]"></div>
+        <div className="hero bg-banner bg-base-200 mt-6 min-h-[600px]"></div>
     );
 };
 
